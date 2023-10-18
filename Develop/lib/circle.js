a2406d9f34a28ddd310e4c268daf02630d3d0545
cj cabrera <cabrera012500@gmail.com>
2023-10-17 21:00:09 -0500

    created circle class and edited index.js

---
+++ Develop/lib/circle.js
@@ -0,0 +1,16 @@
+class Circle {
+    constructor(color) {
+      this.color = color;
+    }
+  
+    render(text, textColor) {
+      // Logic to generate SVG for a circle with the provided parameters
+      const createCircle = `<svg width="300" height="200">
+        <circle cx="150" cy="100" r="50" fill="${this.color}" />
+        <text x="50%" y="50%" text-anchor="middle" fill="${textColor}">${text}</text>
+      </svg>`;
+      return createCircle;
+    }
+  }
+  
+  module.exports = Circle;
---
+++ index.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const Circle = require('./Develop/lib/circle');  // Import the Circle class
+const Triangle = require('./Develop/lib/triangle');  // Import the triangle class
+const Square = require('./Develop/lib/square');  // Import the square class
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'Enter up to three characters: ',
+      name: 'text',
+    },
+    {
+      type: 'input',
+      message: 'Enter desired text color OR a hexadecimal number: ',
+      name: 'textColor',
+    },
+    {
+      type: 'list',
+      message: 'Select desired shape: ',
+      choices: ['Circle', 'Triangle', 'Square'],
+      name: 'shape',
+    },
+    {
+      type: 'input',
+      message: 'Enter desired shape color: ',
+      name: 'shapeColor',
+    },
+  ])
+
+  .then((answers) => {
+    let shape;
+    // handle shape classes here
+    switch (answers.shape.toLowerCase()) {
+      case 'circle':
+        shape = new Circle(answers.shapeColor);  // Use Circle class for 'circle'
+        break;
+
+
+      default:
+        console.log('Invalid shape choice.');
+        return;
+    }
+
+    const svgLogo = shape.render(answers.text, answers.textColor);
+    fs.writeFileSync('logo.svg', svgLogo);
+    console.log('Generated logo.svg');
+  })
+  .catch((error) => {
+    console.error('An error occurred:', error);
+  });
